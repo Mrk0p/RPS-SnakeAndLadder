a5c059b1b927ededfeac0517ce2d2b798f6399db
Mrk0p <sachinwele23@gamil.com>
2023-06-22 19:52:31 +0530

    Added:tell player won the game

--- src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
@@ -4,47 +4,57 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 
+    static int player1 = 0;
+    static int player2 = 0;
 
     public static void main(String[] args) {
 
-        int position = 0;
-        int player = 0;
-        int count = 0;
-
-        System.out.println("Player Starting Position will be:~ ");
-        System.out.println(+position);
-
         Random random = new Random();
-        while (player!= 100) {
-            count++;
+        int diceRoll1 = 0;
+        int diceRoll2 = 0;
+        int numDiceRoll1 = 0;
+        int numDiceRoll2 = 0;
 
-            int dice = random.nextInt(6+1);
-            System.out.println("dice roll result: "+dice);
+        while (player1 != 100 && player2 != 100) {
 
-            int cases = random.nextInt(3);
+            diceRoll1 = random.nextInt(6 + 1);
+            numDiceRoll1++;
+            System.out.println("Number of time dice roll by player 1 is :" + numDiceRoll1);
+            player1 = diceRoll1 + player1;
+            System.out.println("Number obtain after dice roll by player 1 is :" + diceRoll1);
 
-            switch (cases) {
-                case 0:
-                    System.out.println("No Play!!");
-                    break;
+            diceRoll2 = random.nextInt(6 + 1);
+            numDiceRoll2++;
+            System.out.println("Number of time dice roll by player 2 is:" + numDiceRoll2);
+            player2 = diceRoll2 + player2;
+            System.out.println("Number obtain after dice roll by player 2 is :" + diceRoll2);
 
-                case 1:
-                    System.out.println("Ladder!");
-                    player = player+ dice;
-                    if(player > 100)
-                    {
-                        player = dice;
-                    }
-                    break;
+            if (player1 == 11 || player1 == 24 || player1 == 39 || player1 == 48 ||
+                    player1 == 60 || player1 == 70 || player1 == 79 || player1 == 85) {
+                System.out.println("Player 1 got a ladder :" + player1);
+            } else if (player1 == 17 || player1 == 30 || player1 == 38 || player1 == 54 || player1 == 69 ||
+                    player1 == 78 || player1 == 82) {
+                System.out.println("Player 1 beaten by snake :" + player1);
+            } else if (player2 == 11 || player2 == 24 || player2 == 39 || player2 == 48 ||
+                    player2 == 60 || player2 == 70 || player2 == 79 || player2 == 85) {
+                System.out.println("Player 2 got a ladder " + player2);
+            } else if (player2 == 17 || player2 == 30 || player2 == 38 || player2 == 54 || player2 == 69 ||
+                    player2 == 78 || player2 == 82) {
+                System.out.println("Player 2 beaten by snake :" + player2);
+            } else
+                System.out.println("Not a snake or ladder..");
 
-                default:
-                    System.out.println("Snake!");
-                    player = player - dice;
+
+            if (player1 > 100) {
+                player1 = player1 - diceRoll1;
             }
-            System.out.println("Position of the player: " + player);
-        }
-        System.out.println("Player roll dice "+count+" times to win.");
+            if (player2 > 100) {
+                player2 = player2 - diceRoll2;
+            }
+            System.out.println("player 1 :" + player1);
+            System.out.println("player 2 :" + player2);
 
+        }
 
     }
 }
