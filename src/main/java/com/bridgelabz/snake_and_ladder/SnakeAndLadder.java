50c3318ab42d8a3d322519ae28dbaa1c2df99443
Mrk0p <sachinwele23@gamil.com>
2023-06-22 19:41:53 +0530

    Added:player rolls the dice

--- src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.snake_and_ladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int position = 0;
-        System.out.println("player at start:"+position);
+        Random random = new Random();
+
+        int diceRoll = random.nextInt(6+1);
+        System.out.println("Dice roll:"+diceRoll);
     }
 }
