be03d5d3b4461d02dc989a53c03946dc0f29304d
Mrk0p <sachinwele23@gamil.com>
2023-06-22 19:46:49 +0530

    Added:player at exact winning position

--- src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
@@ -5,38 +5,42 @@ import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int position = 0;
-        int count = 0;
         int player = 0;
+        int count = 0;
 
-        System.out.println("player at start :");
-        System.out.println(position);
-
+        System.out.println("Player Starting Position will be:~ ");
+        System.out.println(+position);
 
         Random random = new Random();
-        while (player != 100) {
+        while (player!= 100) {
             count++;
 
             int dice = random.nextInt(6+1);
-            System.out.println("dice roll : "+dice);
+            System.out.println("dice roll result: "+dice);
 
-            int choice = random.nextInt(3);
+            int cases = random.nextInt(3);
 
-            switch (choice) {
+            switch (cases) {
                 case 0:
                     System.out.println("No Play!!");
                     break;
 
                 case 1:
                     System.out.println("Ladder!");
-                    player = player + dice;
+                    player = player+ dice;
+                    if(player > 100)
+                    {
+                        player = dice;
+                    }
                     break;
 
                 default:
                     System.out.println("Snake!");
                     player = player - dice;
             }
-            System.out.println(" Position of the player: " + player);
+            System.out.println("Position of the player: " + player);
         }
         System.out.println("Player roll dice "+count+" times to win.");
+
     }
 }
