fe0813b550dd55ca2dc46e8ebb7c2d369e49dea5
Mrk0p <sachinwele23@gamil.com>
2023-06-22 19:45:28 +0530

    Added:till the player reaches to winning

--- src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
@@ -4,37 +4,39 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-
         int position = 0;
+        int count = 0;
+        int player = 0;
 
         System.out.println("player at start :");
-        System.out.println(+position);
+        System.out.println(position);
+
 
         Random random = new Random();
-        int dice = random.nextInt(6+1);
-        System.out.println("dice roll: "+dice);
+        while (player != 100) {
+            count++;
 
-        int userPosition = 0;
-        int choies = random.nextInt(3);
+            int dice = random.nextInt(6+1);
+            System.out.println("dice roll : "+dice);
 
-        switch (choies) {
-            case 0:
-                System.out.println("No Play");
-                break;
+            int choice = random.nextInt(3);
 
-            case 1:
-                System.out.println("Ladder");
-                userPosition = userPosition + dice;
-                break;
+            switch (choice) {
+                case 0:
+                    System.out.println("No Play!!");
+                    break;
 
-            default:
-                System.out.println("Snake");
-                userPosition = userPosition - dice;
+                case 1:
+                    System.out.println("Ladder!");
+                    player = player + dice;
+                    break;
 
-                if (userPosition < 0) {
-                    userPosition = 0;
-                }
+                default:
+                    System.out.println("Snake!");
+                    player = player - dice;
+            }
+            System.out.println(" Position of the player: " + player);
         }
-        System.out.println("Present Position of the Player : " + userPosition);
+        System.out.println("Player roll dice "+count+" times to win.");
     }
 }
