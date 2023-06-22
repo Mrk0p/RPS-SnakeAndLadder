d15d54dcc212c603f6e331540007363d50940ef5
Mrk0p <sachinwele23@gamil.com>
2023-06-22 19:43:53 +0530

    Added:checks option for player to move

--- src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snake_and_ladder/SnakeAndLadder.java
@@ -4,9 +4,37 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        Random random = new Random();
 
-        int diceRoll = random.nextInt(6+1);
-        System.out.println("Dice roll:"+diceRoll);
+        int position = 0;
+
+        System.out.println("player at start :");
+        System.out.println(+position);
+
+        Random random = new Random();
+        int dice = random.nextInt(6+1);
+        System.out.println("dice roll: "+dice);
+
+        int userPosition = 0;
+        int choies = random.nextInt(3);
+
+        switch (choies) {
+            case 0:
+                System.out.println("No Play");
+                break;
+
+            case 1:
+                System.out.println("Ladder");
+                userPosition = userPosition + dice;
+                break;
+
+            default:
+                System.out.println("Snake");
+                userPosition = userPosition - dice;
+
+                if (userPosition < 0) {
+                    userPosition = 0;
+                }
+        }
+        System.out.println("Present Position of the Player : " + userPosition);
     }
 }
